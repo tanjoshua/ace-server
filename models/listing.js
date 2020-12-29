f45f1d562478127c1a045e5d703bbbd4792b2f8f
tanjoshua <jtanjoshua@gmail.com>
2020-12-29 14:09:39 +0800

    listing and tutor schemas

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    tutor: {
+      type: mongoose.Types.ObjectId,
+      ref: "Tutor",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Listing", listingSchema);
