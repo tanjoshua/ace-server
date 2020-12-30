e7b86ae00bf03cebc496c6fc63f9155a3b1427a8
tanjoshua <jtanjoshua@gmail.com>
2020-12-30 18:09:03 +0800

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 // node imports
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // internal imports
 const config = require("../config"); // not stored in repo
@@ -34,4 +35,39 @@ exports.signup = (req, res, next) => {
 };
 
 // login
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User not found");
+        error.statusCode = 401; // or 404
+        return next(error);
+      }
+
+      bcrypt.compare(password, user.password).then((result) => {
+        if (!result) {
+          const error = new Error("Wrong password");
+          error.statusCode = 401; // or 404
+          return next(error);
+        }
+
+        // create jwt
+        const token = jwt.sign(
+          {
+            name: user.name,
+            email: user.email,
+            userId: user._id.toString(),
+          },
+          config.JWT_KEY
+        );
+
+        res.json({ token, userId: user._id.toString() });
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
