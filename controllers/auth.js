e12569d70a592ac8230203bf5fb1acc254301bab
tanjoshua <jtanjoshua@gmail.com>
2020-12-30 15:40:16 +0800

    user signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,37 @@
 // node imports
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
 // internal imports
+const config = require("../config"); // not stored in repo
 const User = require("../models/user");
 
 // signup
-exports.signup = (req, res, next) => {};
+exports.signup = (req, res, next) => {
+  // checking validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Validation failed");
+    err.statusCode = 422;
+    return next(err);
+  }
+
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then((result) => {
+      const user = new User({ email, name, password: result });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
 
 // login
 exports.login = (req, res, next) => {};
--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,35 @@
 const express = require("express");
 const { body } = require("express-validator");
 
-// controller imports
+// internal imports
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
 // POST /auth/signup
-router.get("/signup", authController.signup);
+router.post(
+  "/signup",
+  // validation
+  [
+    body("email")
+      .isEmail()
+      .custom((value) => {
+        // check if email is already used
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("Email already exists");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 6 }),
+    body("name").trim().notEmpty().isAlpha(),
+  ],
+  authController.signup
+);
 
 // POST /auth/login
-router.get("/login", authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
