fb6543b3fa066a45c1d0b0d75ee4f90d0e97b057
tanjoshua <jtanjoshua@gmail.com>
2021-01-16 11:46:50 -0500

    ratings for tutors

--- models/users/tutor.js
+++ models/users/tutor.js
@@ -12,6 +12,25 @@ const tutorSchema = Schema({
       ref: "Listing",
     },
   ],
+  totalRating: {
+    type: Number,
+    default: 0,
+  },
+  ratingCount: {
+    type: Number,
+    default: 0,
+  },
 });
+
+// compute average rating
+tutorSchema.virtual("averageRating").get(() => {
+  if (!this.ratingCount) {
+    return this.totalRating / this.ratingCount;
+  } else {
+    return 0;
+  }
+});
+tutorSchema.set("toObject", { virtuals: true });
+tutorSchema.set("toJSON", { virtuals: true });
 
 module.exports = User.discriminator("Tutor", tutorSchema);
